ce72c059faeb5ac4e317fded97f396640546f76e
Muthuvijayaraja <tmuthuvijayaraja@yahoo.com>
2019-10-29 10:32:26 +0530

    commit muthu-29.10.19

--- app/src/main/java/org/reginpaul/fragment/AboutusFragment.java
+++ app/src/main/java/org/reginpaul/fragment/AboutusFragment.java
@@ -7,66 +7,57 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
-import android.widget.ViewFlipper;
+import android.widget.RelativeLayout;
 import org.reginpaul.R;
 
 public class AboutusFragment extends Fragment {
 
-    private ViewFlipper v_flipper;
-    private ImageView Mail,Phone,Rate;
+    private ImageView v_flipper;
+    private RelativeLayout mail, rate;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, final Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_about_us, container, false);
 
-        int image[]={R.drawable.ad1,R.drawable.ad2,R.drawable.ad3,R.drawable.ad4,R.drawable.ad5};
-        v_flipper=rootView.findViewById(R.id.v_flipper);
-        Mail=(ImageView) rootView.findViewById(R.id.mail);
-//        Phone=(ImageView) rootView.findViewById(R.id.ph);
-        Rate=(ImageView) rootView.findViewById(R.id.rate);
-        for(int images : image){
-            flipperImages(images);
-        }
-        Mail.setOnClickListener(new View.OnClickListener() {
+        int image[] = {R.drawable.ad1, R.drawable.ad2, R.drawable.ad3, R.drawable.ad4, R.drawable.ad5};
+        v_flipper = rootView.findViewById(R.id.v_flipper);
+        mail = rootView.findViewById(R.id.srow1);
+        rate = rootView.findViewById(R.id.srow2);
+
+        Animation animation = new AlphaAnimation(1, 0);
+        animation.setDuration(1000);
+        animation.setInterpolator(new LinearInterpolator());
+        animation.setRepeatCount(Animation.INFINITE);
+        animation.setRepeatMode(Animation.REVERSE);
+        v_flipper.startAnimation(animation);
+
+        mail.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent (Intent.ACTION_VIEW , Uri.parse("mailto:" + "rejinpaul@rejinpaul.com"));
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("mailto:" + "nrrejinpaul@gmail.com"));
                 intent.putExtra(Intent.EXTRA_SUBJECT, "Queries to Rejinpaul");
 //                intent.putExtra(Intent.EXTRA_TEXT, "your_text");
                 startActivity(intent);
             }
         });
-//        Phone.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent callIntent = new Intent(Intent.ACTION_DIAL);
-//                callIntent.setData(Uri.parse("tel:1234567890"));
-//                startActivity(callIntent);
-//            }
-//        });
-Rate.setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View v) {
-        final String appPackageName = getActivity().getPackageName(); // getPackageName() from Context or Activity object
-        try {
-            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
-        } catch (android.content.ActivityNotFoundException anfe) {
-            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
-        }
-    }
-});
+        rate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String appPackageName = getActivity().getPackageName(); // getPackageName() from Context or Activity object
+                try {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+                } catch (android.content.ActivityNotFoundException anfe) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+                }
+            }
+        });
         return rootView;
     }
 
-    public void flipperImages(int image){
-        ImageView imageView=new ImageView(getContext());
-        imageView.setBackgroundResource(image);
-        v_flipper.addView(imageView);
-        v_flipper.setFlipInterval(5000);
-        v_flipper.setAutoStart(true);
-        v_flipper.setInAnimation(getContext(),android.R.anim.slide_in_left);
-        v_flipper.setOutAnimation(getContext(),android.R.anim.slide_out_right);
-    }
+
 }
